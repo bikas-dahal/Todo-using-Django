760f2ed6ed0fc045aca0cb0baef1093fb3ccb7de
bikky <bikkydahal@gmail.com>
2024-07-09 08:32:29 +0545

    Minor changes for prod setting(railway)

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -179,7 +179,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
 
 
@@ -199,7 +201,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # For compressor to work
 
-COMPRESS_ROOT = BASE_DIR / 'static'
+# COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 STATICFILES_FINDERS = [
